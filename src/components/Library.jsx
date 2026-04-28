8064c15c6e52d669a9cc34b13e34611560881ae7
ngetnet559-web <ngetnet559@gmail.com>
2026-04-28 04:15:55 -0900

    implementing library section

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -3,13 +3,72 @@ import assets from '../assets/assets'
 
 const Library = () => {
   return (
-    <div>
+    <div className="relative w-full bg-cover  bg-center px-4 py-4"
+    >
+      <div className='absolute inset-0 bg-[#c0bdb8]/60'></div>
+      <div className="relative  z-50 text-center mb-4">
         <h1>Our Library</h1>
         <p>Knowledge Awaits you </p>
 
-        <div className="">
-            <div className="w-30 h-auto rounded-2xl bg-black "></div>
         </div>
+
+        <div className="relative grid grid-cols-2 gap-4 ml-10 z-50">
+  <div className="w-32 rounded-2xl p-4 bg-white shadow-md">
+  <img
+    className="w-20 h-20 object-cover rounded-xl mx-auto"
+    src={assets.b1}
+    alt=""
+  />
+
+  <h3 className="mt-3 font-semibold text-center">The Great Gatsby</h3>
+
+  <p className="text-sm text-gray-700 mt-1 text-center">A classic novel</p>
+</div>
+<div className="w-32 rounded-2xl p-4 bg-white shadow-md">
+  <img
+    className="w-20 h-20 object-cover rounded-xl mx-auto"
+    src={assets.b1}
+    alt=""
+  />
+
+  <h3 className="mt-3 font-semibold text-center">The Great Gatsby</h3>
+
+  <p className="text-sm text-gray-700 mt-1 text-center">A classic novel</p>
+</div>
+<div className="w-32 rounded-2xl p-4 bg-white shadow-md">
+  <img
+    className="w-20 h-20 object-cover rounded-xl mx-auto"
+    src={assets.b1}
+    alt=""
+  />
+
+  <h3 className="mt-3 font-semibold text-center">The Great Gatsby</h3>
+
+  <p className="text-sm text-gray-700 mt-1 text-center">A classic novel</p>
+</div>
+<div className="w-32 rounded-2xl p-4 bg-white shadow-md">
+  <img
+    className="w-20 h-20 object-cover rounded-xl mx-auto"
+    src={assets.b1}
+    alt=""
+  />
+
+  <h3 className="mt-3 font-semibold text-center">The Great Gatsby</h3>
+
+  <p className="text-sm text-gray-700 mt-1 text-center">A classic novel</p>
+</div>
+<div className="w-32 rounded-2xl p-4 bg-white shadow-md">
+  <img
+    className="w-20 h-20 object-cover rounded-xl mx-auto"
+    src={assets.b1}
+    alt=""
+  />
+
+  <h3 className="mt-3 font-semibold text-center">The Great Gatsby</h3>
+
+  <p className="text-sm text-gray-700 mt-1 text-center">A classic novel</p>
+</div>
+</div>
     </div>
   )
 }
